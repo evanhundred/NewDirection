0e898ffb95cc3ef4e21bfdb3517e41efb7c3a381
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2024-02-14 10:25:59 -0500

    complete basic single event

--- src/index.js
+++ src/index.js
@@ -14,7 +14,37 @@ const render = () => {
   titleContainer.append(h1);
   h1.innerText = "N D";
 
-  App.append(logoContainer, titleContainer);
+  const expandEvents = (e) => {
+    e.preventDefault();
+    eventsContainer.append(eventsContent);
+  };
+
+  const eventsContainer = document.createElement("div");
+  eventsContainer.id = "events-container";
+  const eventsH2 = document.createElement("h2");
+  eventsContainer.append(eventsH2);
+  eventsH2.innerText = "events";
+  eventsH2.addEventListener("click", expandEvents);
+
+  const eventsContent = document.createElement("div");
+  eventsContent.className = "content";
+  // const eventsArray = [];
+
+  let newEvent = document.createElement("div");
+  eventsContent.append(newEvent);
+  newEvent.className = "new-event";
+  let header = document.createElement("h3");
+  header.innerText = "Black History Celebration";
+  newEvent.append(header);
+
+  let text = document.createElement("p");
+  text.innerText = "2/24/2024 - 5 PM";
+  newEvent.append(text);
+  text = document.createElement("p");
+  text.innerText = "Queens, NY";
+  newEvent.append(text);
+
+  App.append(logoContainer, titleContainer, eventsContainer);
 
   const root = document.getElementById("root");
   root.append(App);
