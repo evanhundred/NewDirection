fa160003d17ee1ad29901f31360da7494217e092
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2024-02-14 09:29:41 -0500

    create styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const render = () => {
-	const App = document.createElement('div');
-	App.innerText = 'New Direction';
+  const App = document.createElement("div");
+  App.id = "app-container";
 
-	const root = document.getElementById('root');
-	root.append(App);
-}
+  const logoContainer = document.createElement("div");
+  logoContainer.className = "logo-container";
+  const logoImg = document.createElement("img");
+  logoContainer.append(logoImg);
+  logoImg.src = "../assets/nd_logo.png";
+
+  const h1 = document.createElement("h1");
+  h1.innerText = "N D";
+
+  App.append(logoContainer, h1);
+
+  const root = document.getElementById("root");
+  root.append(App);
+};
 
 document.addEventListener("DOMContentLoaded", () => {
-	render();
-})
+  render();
+});
