a1165a4c73f5abf72e7ad59d68a9b0be31fd3c45
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2024-02-17 11:40:41 -0500

    create media query to use insta link at 600px width

--- src/index.js
+++ src/index.js
@@ -66,11 +66,30 @@ const render = () => {
   instagram.id = "instagram-container";
   let a = document.createElement("a");
   instagram.append(a);
-  a.href = "instagram://user?username=newdirection.music";
+
+  let maxWatch = window.matchMedia("(max-width: 600px)");
+
+  const makeLink = (e, a) => {
+    console.log(maxWatch);
+    console.log(e);
+    let instaUrl;
+    if (maxWatch.matches) {
+      instaUrl = "instagram://user?username=newdirection.music";
+    } else {
+      instaUrl = "https://www.instagram.com/newdirection.music";
+    }
+    a.href = instaUrl;
+  };
+
+  makeLink(null, a);
+  maxWatch.addEventListener("change", (e) => makeLink(e, a));
+
+  // a.href = "instagram://user?username=newdirection.music";
   // a.href = "https://www.instagram.com/_u/newdirection.music";
   // a.href = "https://www.instagram.com/newdirection.music";
   a.target = "_blank";
-  rel = "noopener noreferrer";
+  a.rel = "noopener noreferrer";
+
   let img = document.createElement("img");
   a.append(img);
   img.src = "./assets/instagram.png";
