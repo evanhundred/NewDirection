608dd1a3cbd5d728912e853d3f3826bad56964ee
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2024-02-22 15:39:25 -0500

    style icon

--- src/index.js
+++ src/index.js
@@ -116,16 +116,18 @@ const render = () => {
     '<a href="https://www.flaticon.com/free-icons/instagram-logo" title="instagram logo icons">Instagram logo icon created by Freepik - Flaticon</a>';
 
   const ndIcon = document.createElement("div");
-
   ndIcon.id = "icon";
+  div = document.createElement("div");
+  div.className = "img-container";
+  ndIcon.append(div);
   img = document.createElement("img");
   img.id = "n";
   img.src = "./assets/icon/letter-n.png";
-  ndIcon.append(img);
+  div.append(img);
   img = document.createElement("img");
   img.id = "d";
   img.src = "./assets/icon/letter-d.png";
-  ndIcon.append(img);
+  div.append(img);
 
   contentContainer.append(
     logoContainer,
