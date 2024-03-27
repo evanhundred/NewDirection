30b57847319bc49a349d4713863b87e75892015d
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2024-03-27 10:34:54 -0400

    shift a tag

--- src/footer/index.js
+++ src/footer/index.js
@@ -1,10 +1,10 @@
 const Footer = () => {
-  const footer = document.createElement("div");
-  footer.id = "footer";
-  let p = document.createElement("p");
+  const footer = document.createElement('div');
+  footer.id = 'footer';
+  let p = document.createElement('p');
   footer.append(p);
   p.innerHTML =
-    '<a href="https://www.flaticon.com/free-icons/instagram-logo" title="instagram logo icons">Instagram logo icon created by Freepik - Flaticon</a>';
+    'Instagram logo icon created by <a href="https://www.flaticon.com/free-icons/instagram-logo" title="instagram logo icons">Freepik - Flaticon</a>';
 
   return footer;
 };
